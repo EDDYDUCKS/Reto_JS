0eaaacc04c2d464d1a6b081c2e35a806d3d556cc
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-02-16 19:21:27 -0600

    Iniciando con el Js

---
+++ app.js
@@ -0,0 +1,11 @@
+const datosHorario = [
+    { codigo: '0413', asignatura: 'PROGRAMACION WEB', grupo: 'Gpo1', dia: 'MARTES', horario: '10:00 - 11:40', aula: 'E201', color: 'table-primary' },
+    { codigo: '0413', asignatura: 'PROGRAMACION WEB', grupo: 'Gpo1', dia: 'JUEVES', horario: '15:00 - 16:40', aula: 'E201', color: 'table-primary' },
+    { codigo: '0402', asignatura: 'INTRO. INGENIERIA', grupo: 'Gpo3', dia: 'LUNES', horario: '08:00 - 09:40', aula: 'D104', color: 'table-success' },
+    { codigo: '0402', asignatura: 'INTRO. INGENIERIA', grupo: 'Gpo3', dia: 'MIERCOLES', horario: '08:00 - 09:40', aula: 'D104', color: 'table-success' },
+    { codigo: '0402', asignatura: 'INTRO. INGENIERIA', grupo: 'Gpo4', dia: 'LUNES', horario: '10:00 - 11:40', aula: 'E201', color: 'table-success' },
+    { codigo: '0402', asignatura: 'INTRO. INGENIERIA', grupo: 'Gpo4', dia: 'JUEVES', horario: '08:00 - 09:40', aula: 'E201', color: 'table-success' }
+];
+
+const diasSemana = ['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES'];
+const bloquesHorarios = ['08:00 - 09:40', '10:00 - 11:40', '13:00 - 14:40', '15:00 - 16:40'];
